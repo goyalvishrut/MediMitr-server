884ccdbde37ecf12b70e7e3222939e2877a1b378
goyalvishrut <goyalvishrut@gmail.com>
2025-04-01 15:48:52 +0530

    jwt

--- server/src/main/kotlin/org/example/medimitr/medicines/MedicineRouting.kt
+++ server/src/main/kotlin/org/example/medimitr/medicines/MedicineRouting.kt
@@ -1,6 +1,7 @@
 package org.example.medimitr.medicines
 
 import io.ktor.server.application.Application
+import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
@@ -14,17 +15,19 @@ fun Application.configureMedicineRouting() {
     val medicineService by inject<MedicineService>()
 
     routing {
-        get("/medicines") {
-            // GET /medicines endpoint
-            val medicines = medicineService.getAllMedicine() // Get all medicines
-            call.respond(medicines) // Respond with medicine list
-        }
+        authenticate {
+            get("/medicines") {
+                // GET /medicines endpoint
+                val medicines = medicineService.getAllMedicine() // Get all medicines
+                call.respond(medicines) // Respond with medicine list
+            }
 
-        post("/medicines") {
-            // POST /medicines endpoint
-            val newMedicine = call.receive<NewMedicine>() // Receive new medicine data
-            val medicine = medicineService.createMedicine(newMedicine) // Create medicine
-            call.respond(medicine) // Respond with created medicine
+            post("/medicines") {
+                // POST /medicines endpoint
+                val newMedicine = call.receive<NewMedicine>() // Receive new medicine data
+                val medicine = medicineService.createMedicine(newMedicine) // Create medicine
+                call.respond(medicine) // Respond with created medicine
+            }
         }
     }
 }
